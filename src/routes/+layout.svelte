84e4b333b8b904d40042afa6ec47873d37793a72
nareshix <nareshix66@gmail.com>
2025-06-21 02:28:17 +0800

    init Toast to be used accorss pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
-	import "../app.css";
-	import { ModeWatcher } from "mode-watcher";
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	import { Button } from '$lib/components/ui/button';
 	let { children } = $props();
-   </script>
-	
-   <ModeWatcher />
-   {@render children?.()}
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<!-- <Button onclick={toggleMode} variant="outline" size="icon">
+	<SunIcon
+		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+	/>
+	<MoonIcon
+		class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button> -->
+<Toaster richColors closeButton />
+<ModeWatcher />
+{@render children?.()}
