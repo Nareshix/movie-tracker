6b791a3764c4b068209a6501b6c14ed252ccb559
nareshix <nareshix66@gmail.com>
2025-06-23 17:56:37 +0800

    verifies email by checking witht he temp db

---
+++ src/routes/(auth)/verify-email/+page.server.ts
@@ -0,0 +1,36 @@
+import { query } from '$lib/server/db.js';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types.js';
+
+const ADD_USER_AFTER_VERIFICATION = `
+    WITH moved_user AS (
+        DELETE FROM unverified_users
+        WHERE token = $1 AND user_id = $2
+        RETURNING email, hashed_password
+    )
+    INSERT INTO users (email, hashed_password)
+    SELECT email, hashed_password FROM moved_user;
+`;
+
+export const load: PageServerLoad = async ({ url }) => {
+	const token = url.searchParams.get('token');
+	const user_id = url.searchParams.get('user_id');
+	let result;
+	try {
+		 result = await query(ADD_USER_AFTER_VERIFICATION, [token, user_id]);
+	} catch (err) {
+		// log it
+		console.log(err)
+		return {
+			error: 'There is an error in server. Please try again'
+		}
+	}
+
+	if (result.rowCount === 0) {
+		return {
+			error: 'This verification link is invalid or has expired.'
+		};
+	}
+	redirect(308, '/');
+
+};
