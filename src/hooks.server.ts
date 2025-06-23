f3eff52e67be4719520762c3bf67c361f8de7d1b
nareshix <nareshix66@gmail.com>
2025-06-23 22:57:03 +0800

    changed status code from 301 to 307. 301 is permanent and was cached by browser (problematic). 307 better as it is temporary

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,7 +5,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     const protectedRoutes = ['/discover', '/search', '/home', '/lists'];
 
     if (protectedRoutes.includes(event.url.pathname)) {
-		redirect(301, '/login');
+		redirect(307, '/login');
 	}
 
 	const response = await resolve(event);
