a76b9a8ec2237c2d4df2ca446c71aa04ed6506b9
nareshix <nareshix66@gmail.com>
2025-06-24 13:13:17 +0800

    proteced routes. can only enter if session is valid

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,19 @@
+import { validateSessionToken } from '$lib/session';
 import { redirect, type Handle } from '@sveltejs/kit';
 
-
 export const handle: Handle = async ({ event, resolve }) => {
-    const protectedRoutes = ['/discover', '/search', '/home', '/lists'];
+	const protectedRoutes = ['/discover', '/search', '/home', '/lists'];
 
-    if (protectedRoutes.includes(event.url.pathname)) {
-		redirect(307, '/login');
+	if (protectedRoutes.includes(event.url.pathname)) {
+		const sessionToken = event.cookies.get('sessionToken');
+		if (!sessionToken) {
+			throw redirect(303, '/login');
+		}
+
+		const sessionValid = await validateSessionToken(sessionToken);
+		if (!sessionValid) {
+			throw redirect(303, '/login');
+		}
 	}
 
 	const response = await resolve(event);
