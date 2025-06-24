6de8fd8207facdc346d94b22880d3d5f9d7350d1
nareshix <nareshix66@gmail.com>
2025-06-24 22:11:42 +0800

    deletes all prev sessions when user successfully resetted password. also with chagned to subqueries as it is easier to read

--- src/routes/(auth)/reset-password/[token]/+page.server.ts
+++ src/routes/(auth)/reset-password/[token]/+page.server.ts
@@ -6,16 +6,20 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
 	try {
 		const result = await query(
 			`
-			 WITH used_token AS (
-				 DELETE FROM reset_password
-				 WHERE reset_password_token = $1
-				 RETURNING user_id, hashed_password
-			 )
-			 UPDATE users
-			 SET hashed_password = used_token.hashed_password
-			 FROM used_token
-			 WHERE users.id = used_token.user_id
-			 RETURNING users.id;`,
+				WITH used_token AS (
+					DELETE FROM reset_password
+					WHERE reset_password_token = $1
+					RETURNING user_id, hashed_password
+				),
+				deleted_sessions AS (
+					DELETE FROM session
+					WHERE user_id = (SELECT user_id FROM used_token)
+					RETURNING user_id
+				)
+				UPDATE users
+				SET hashed_password = (SELECT hashed_password FROM used_token)
+				WHERE id = (SELECT user_id FROM used_token)
+				RETURNING users.id;`,
 			[params.token]
 		);
 
@@ -24,10 +28,10 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
 				errorMsg: 'This reset password link is invalid or has expired. Please try again.'
 			};
 		}
-		const user_id = result.rows[0].id
+		const user_id = result.rows[0].id;
 		const session = await createSession(user_id);
 		cookies.set('sessionToken', session.token, { path: '/' });
-	
+
 		return { success: true };
 	} catch (err) {
 		// log it
