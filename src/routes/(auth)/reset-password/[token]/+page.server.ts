32c9167705aa0bcd6f6f55872cc6a25aaa552283
nareshix <nareshix66@gmail.com>
2025-06-24 17:09:40 +0800

    testing querying db to check whether rest passwrod token exist in temp db

--- src/routes/(auth)/reset-password/[token]/+page.server.ts
+++ src/routes/(auth)/reset-password/[token]/+page.server.ts
@@ -1,6 +1,23 @@
+import { query } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ params }) => {
+export const load: PageServerLoad = async ({ params }) => {
+	try {
+		const x = await query('SELECT 1 FROM reset_password WHERE reset_password_token = $1 ', [
+			params.token
+		]);
+
+		if (x.rowCount === 0) {
+			console.log('FAILIRUE')
+		}
+		console.log(x);
+
+	} catch (err) {
+		// log it
+		console.log(err);
+	}
+
 	return {
 		post: {
 			title: `Title for ${params.token} goes here`,
