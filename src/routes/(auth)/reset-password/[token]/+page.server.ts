fe13a281bba5a515520e21f0187f7d9164f27647
nareshix <nareshix66@gmail.com>
2025-06-24 19:04:32 +0800

    if token is same as in temp db, it updates the old password with new one and deletes the data from reset_password column

--- src/routes/(auth)/reset-password/[token]/+page.server.ts
+++ src/routes/(auth)/reset-password/[token]/+page.server.ts
@@ -1,27 +1,32 @@
 import { query } from '$lib/server/db';
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	try {
-		const x = await query('SELECT 1 FROM reset_password WHERE reset_password_token = $1 ', [
-			params.token
-		]);
+		const result = await query(
+			`
+			 WITH used_token AS (
+				 DELETE FROM reset_password
+				 WHERE reset_password_token = $1
+				 RETURNING user_id, hashed_password
+			 )
+			 UPDATE users
+			 SET hashed_password = used_token.hashed_password
+			 FROM used_token
+			 WHERE users.id = used_token.user_id
+			 RETURNING users.id;`,
+			[params.token]
+		);
 
-		if (x.rowCount === 0) {
-			console.log('FAILIRUE')
+		if (result.rowCount === 0) {
+			return {
+				errorMsg: 'This reset password link is invalid or has expired. Please try again.'
+			};
 		}
-		console.log(x);
-
+		return { success: true };
 	} catch (err) {
 		// log it
 		console.log(err);
+		return { errorMsg: 'An unexpected error occurred. Please try again later.' };
 	}
-
-	return {
-		post: {
-			title: `Title for ${params.token} goes here`,
-			content: `Content for ${params.token} goes here`
-		}
-	};
 };
