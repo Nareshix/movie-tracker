426a5e6012aa6fc6b4479ec23a4c32315633bc1a
nareshix <nareshix66@gmail.com>
2025-06-24 11:32:54 +0800

    added a small note and prettified the code

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -2,14 +2,15 @@ import { query } from '$lib/server/db';
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import bcrypt from 'bcrypt';
-import { createSession } from './session';
+import { createSession } from '$lib/session';
 
 export const actions = {
-	login: async ({ request }) => {
+	login: async ({ request, cookies }) => {
 		const data = await request.formData();
 		const email = data.get('email') as string;
 		const password = data.get('password') as string;
 
+		// checks if password is correct with db, then create a session
 		try {
 			const result = await query('SELECT hashed_password FROM users WHERE email=$1', [email]);
 
@@ -22,19 +23,15 @@ export const actions = {
 			const isMatch = await bcrypt.compare(password, hashed_password);
 
 			if (isMatch) {
-				createSession();
-				console.log('success');
+				const session = await createSession();
+				cookies.set('sessionToken', session.token, { path: '/' });
 			} else {
 				return fail(400, {
 					ErrorMsg: 'Wrong Password'
 				});
 			}
-
 		} catch (err) {
 			console.log(err);
 		}
-		
-		
-
 	}
 } satisfies Actions;
