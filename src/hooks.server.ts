5def9108c8cf08f0051c394af86b424e53ee556b
nareshix <nareshix66@gmail.com>
2025-06-24 19:08:46 +0800

    once authenticated, (auth) routes will auto reroute them to /home

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,23 @@ import { validateSessionToken } from '$lib/session';
 import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const protectedRoutes = ['/discover', '/search', '/home', '/lists'];
+	const mainAppRoutes = ['/discover', '/search', '/home', '/lists'];
+	const authRoutes = [
+		'/login',
+		'/signup',
+		'/reset-password',
+		'/verify-email',
+		'/verify-email-display'
+	];
 
-	if (protectedRoutes.includes(event.url.pathname)) {
+	if (authRoutes.includes(event.url.pathname)) {
+		const sessionToken = event.cookies.get('sessionToken');
+		if (sessionToken) {
+			throw redirect(303, '/home');
+		}
+	}
+
+	if (mainAppRoutes.includes(event.url.pathname)) {
 		const sessionToken = event.cookies.get('sessionToken');
 		if (!sessionToken) {
 			throw redirect(303, '/login');
